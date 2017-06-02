f5f352d11be1b764b971bd220d54557812d1c441
Jozef Izso <jozef.izso@gmail.com>
2017-06-02 23:46:52 +0200

    Script for configuring Windows Server features required to run FogBugz

--- README.md
+++ README.md
@@ -2,6 +2,21 @@
 
 > The **xFogBugz** module contains the DSC resources for configuring Windows Server with FogBugz bug tracking software.
 
+## Installing FogBugz
+
+Before running the configuration script you may have to enable bigger
+payloads for Windows Remote Management.
+
+```powershell
+winrm set winrm/config @{MaxEnvelopeSizekb="8192"}
+```
+
+Run the configuration script `fogbugz.ps1` to install IIS, .NET 2.0 and ASP.NET 3.5:
+
+```powershell
+.\fogbugz.ps1
+```
+
 ## License
 
 MIT License, Copyright (c) 2017 Jozef Izso
---
+++ fogbugz.ps1
@@ -0,0 +1,202 @@
+param(
+  [string[]]$machine = "localhost"
+)
+Configuration FogBugzWebServer
+{
+  Import-DscResource –ModuleName 'PSDesiredStateConfiguration'
+
+  Node $machine
+  {
+    # Web Server (IIS)
+    WindowsFeature Web-Server
+    {
+      Name = "Web-Server"
+      Ensure = "Present"
+    }
+
+    # Common HTTP Features
+    WindowsFeature Web-Common-Http
+    {
+      Name = "Web-Common-Http"
+      Ensure = "Present"
+    }
+
+    # Default Document
+    WindowsFeature Web-Default-Doc
+    {
+      Name = "Web-Default-Doc"
+      Ensure = "Present"
+    }
+
+    # Directory Browsing
+    WindowsFeature Web-Dir-Browsing
+    {
+      Name = "Web-Dir-Browsing"
+      Ensure = "Present"
+    }
+
+    # HTTP Errors
+    WindowsFeature Web-Http-Errors
+    {
+      Name = "Web-Http-Errors"
+      Ensure = "Present"
+    }
+
+    # Static Content
+    WindowsFeature Web-Static-Content
+    {
+      Name = "Web-Static-Content"
+      Ensure = "Present"
+    }
+
+
+    # Health and Diagnostics
+    WindowsFeature Web-Health
+    {
+      Name = "Web-Health"
+      Ensure = "Present"
+    }
+
+    # HTTP Logging
+    WindowsFeature Web-Http-Logging
+    {
+      Name = "Web-Http-Logging"
+      Ensure = "Present"
+    }
+
+
+    # Performance
+    WindowsFeature Web-Performance
+    {
+      Name = "Web-Performance"
+      Ensure = "Present"
+    }
+
+    # Static Content Compression
+    WindowsFeature Web-Stat-Compression
+    {
+      Name = "Web-Stat-Compression"
+      Ensure = "Present"
+    }
+
+
+    # Security
+    WindowsFeature Web-Security
+    {
+      Name = "Web-Security"
+      Ensure = "Present"
+    }
+
+    # Request Filtering
+    WindowsFeature Web-Filtering
+    {
+      Name = "Web-Filtering"
+      Ensure = "Present"
+    }
+
+
+    # .NET Extensibility 3.5
+    WindowsFeature Web-Net-Ext
+    {
+      Name = "Web-Net-Ext"
+      Ensure = "Present"
+    }
+
+    # ASP.NET 3.5
+    WindowsFeature Web-Asp-Net
+    {
+      Name = "Web-Asp-Net"
+      Ensure = "Present"
+    }
+
+
+
+    # ISAPI Extensions
+    WindowsFeature Web-ISAPI-Ext
+    {
+      Name = "Web-ISAPI-Ext"
+      Ensure = "Present"
+    }
+
+    # ISAPI Filters
+    WindowsFeature Web-ISAPI-Filter
+    {
+      Name = "Web-ISAPI-Filter"
+      Ensure = "Present"
+    }
+
+
+    # Management Tools
+    WindowsFeature Web-Mgmt-Tools
+    {
+      Name = "Web-Mgmt-Tools"
+      Ensure = "Present"
+    }
+
+    # IIS Management Console
+    WindowsFeature Web-Mgmt-Console
+    {
+      Name = "Web-Mgmt-Console"
+      Ensure = "Present"
+    }
+
+    # IIS 6 Management Compatibility
+    WindowsFeature Web-Mgmt-Compat
+    {
+      Name = "Web-Mgmt-Compat"
+      Ensure = "Present"
+    }
+
+    # IIS 6 Metabase Compatibility
+    WindowsFeature Web-Metabase
+    {
+      Name = "Web-Metabase"
+      Ensure = "Present"
+    }
+
+
+    # IIS 6 Scripting Tools
+    WindowsFeature Web-Lgcy-Scripting
+    {
+      Name = "Web-Lgcy-Scripting"
+      Ensure = "Present"
+    }
+
+    # IIS 6 WMI Compatibility
+    WindowsFeature Web-WMI
+    {
+      Name = "Web-WMI"
+      Ensure = "Present"
+    }
+
+    # IIS Management Scripts and Tools
+    WindowsFeature Web-Scripting-Tools
+    {
+      Name = "Web-Scripting-Tools"
+      Ensure = "Present"
+    }
+
+
+
+    # .NET Framework 3.5 Features
+    WindowsFeature NET-Framework-Features
+    {
+      Name = "NET-Framework-Features"
+      Ensure = "Present"
+    }
+
+    # .NET Framework 3.5 (includes .NET 2.0 and 3.0)
+    WindowsFeature NET-Framework-Core
+    {
+      Name = "NET-Framework-Core"
+      Ensure = "Present"
+    }
+
+  }
+}
+
+
+# Compile the web server prerequisites
+FogBugzWebServer
+
+Start-DscConfiguration -Path .\FogBugzWebServer -Verbose -Wait
